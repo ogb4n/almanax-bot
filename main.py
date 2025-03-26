6d4c110c6988a02829e6b4aceb18fae415fe58cb
ogb4n <ogb4n@protonmail.com>
2025-03-26 15:32:59 +0100

    le code était dégueulasse et il l'est toujours, mais on s'en branle c'est deprecated issou

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 @bot.event
 async def on_ready():
     print('Bot is ready', bot.user)
-    #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Starting..."))
     try:
         synced = await bot.tree.sync()
         if synced:
@@ -31,35 +30,6 @@ async def on_ready():
         print(f'Error while syncing slash commands: {e}')
     # almanax_retrieve.start()
 
-
-# @tasks.loop(hours=15)
-# async def almanax_retrieve():
-#         await bot.wait_until_ready()
-#         channel = bot.get_channel(int(CHANNEL_ID)) # The channel id that we want the bot to send message
-#         print('Background task started')
-#         print('------')
-#         while not bot.is_closed():
-#             img_options = {
-#                 'format': 'jpg',
-#                 'encoding': "UTF-8",
-#                 'crop-w': '455',
-#                 'crop-h': '250',
-#                 'crop-x': '250',
-#                 'crop-y': '455',
-#                 'user-style-sheet': 'hide.css'
-#             }
-#             img_path = f'almanax-{date.today().strftime("%d-%m-%Y")}.jpg'
-#             imgkit.from_url('http://www.krosmoz.com/fr/almanax', img_path, options=img_options)
-
-#             # Send image to Discord
-#             print('Send a new Almanax day')
-#             file = discord.File(img_path)
-#             await channel.send(file=file)
-
-#             # Delete img file
-#             os.remove(img_path)
-
-@bot.tree.command(name='almanax',description="Vois l'offrande et le bonus du jour")
 async def almanax(interaction: discord.Interaction):
         await interaction.response.defer()
         channel = bot.get_channel(int(CHANNEL_ID)) # The channel id that we want the bot to send message
@@ -83,6 +53,13 @@ async def almanax(interaction: discord.Interaction):
 
         # Delete img file
         os.remove(img_path)
+
+@tasks.loop(hours=15)
+async def almanax_loop():
+        await almanax()
+
+@bot.tree.command(name='almanax',description="Vois l'offrande et le bonus du jour")
+    await almanax()
 
 
 if __name__ == '__main__':
